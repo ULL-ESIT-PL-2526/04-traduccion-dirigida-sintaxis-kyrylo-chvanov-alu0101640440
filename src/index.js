e41301523d99a370ca5affc511d26ed9d734967a
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2026-02-24 18:17:25 +0000

    Add REPL

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
-const parse = require("./parser.js").parse;
+const parse = require('./parser.js').parse;
+const fileSystem = require('fs');
 
-if (process.argv.length >2) {
+function readInput() {
+  process.stdout.write('> ');
+  return fileSystem.readFileSync(0, 'utf8');
+}
+
+function main() {
+  if (process.argv.length > 2) {
     console.log(parse(process.argv[2]));
     process.exit(0);
+  }
+  let input = readInput();
+  while (input) {
+    console.log('\n' + parse(input));
+    input = readInput();
+  }
 }
-console.log(parse("1 - 2"));        // -1
-console.log(parse("10 - 4 - 3"));   // 3
-console.log(parse("7 - 5 - 1"));    // 1
+
+main();
